eaf83ac50a178249717c2b477c21bdf63fc8ccb0
Raj Seth <37039751+raazseth@users.noreply.github.com>
2022-08-29 10:42:25 +0530

    Update YtAction.js

--- Actions/YtAction.js
+++ Actions/YtAction.js
@@ -4,7 +4,7 @@ import { ytConstants } from "./constants";
 export const getBlackFungusYt = (max) => {
   return async (dispatch) => {
     try {
-      const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${max}&q=blackfungus&key=AIzaSyAoVEQqk0UoC9SVv7yoqkaV0Zggkm6ymFY`);
+      const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${max}&q=blackfungus&key=${API_KEY}`);
       dispatch({
         type: ytConstants.GET_BL_FUNGUS_SUCCESS,
         payload: { ytBlackVideo: res },
@@ -18,22 +18,3 @@ export const getBlackFungusYt = (max) => {
   };
 };
 
-// export const getIpAddress = () => {
-//   return async (dispatch) => {
-//     dispatch({
-//       type: ytConstants.GET_IP_REQUEST,
-//     });
-//     try {
-//       const res = await axios.get("http://ip-api.com/json/");
-//       dispatch({
-//         type: ytConstants.GET_IP_SUCESS,
-//         payload: { ip: res.data },
-//       });
-//     } catch (error) {
-//       dispatch({
-//         type: ytConstants.GET_IP_FAILURE,
-//         payload: error.response.data,
-//       });
-//     }
-//   };
-// };
